6d3301a4004a6c85cdc0a25638d9be08a6cb544f
Домашев Данил <D.Domashev@ase-ec.ru>
2023-10-20 16:51:49 +0300

    added db context and base controller

---
+++ MultilayerTemplate.Infrastructure/Database/MultilayerTemplateDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace MultilayerTemplate.Infrastructure.Database;
+
+public class MultilayerTemplateDbContext : DbContext
+{
+    protected readonly IConfiguration Configuration;
+
+    public MultilayerTemplateDbContext(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        // options.UseNpgsql(Configuration.GetConnectionString("MultilayerTemplateDatabase"));
+    }
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(MultilayerTemplateDbContext).Assembly);
+    }
+}
--- MultilayerTemplate/Controllers/BaseController.cs
+++ MultilayerTemplate/Controllers/BaseController.cs
@@ -1,34 +1,12 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using MultilayerTemplate.BLL.Services.Interfaces.Commands;
-using MultilayerTemplate.BLL.Services.Interfaces.Queries;
 
 namespace MultilayerTemplate.Controllers;
 
+/// <summary>
+/// Базовый контроллер (дефолтные аттрибуты и прочее
+/// </summary>
 [Route("api/[controller]/[action]")]
 [ApiController]
-public class BaseController : ControllerBase
+public abstract class BaseController : ControllerBase
 {
-    /// <summary>
-    /// Метод Get контроллера Base
-    /// </summary>
-    /// <param name="id">get Идентификатор сущности</param>
-    /// <returns></returns>
-    [HttpGet]
-    public async Task<IActionResult> GetAsync(int id)
-    {
-        await HttpContext.RequestServices.GetRequiredService<IGetService>().HandleAsync(id);
-        return Ok(id);
-    }
-
-    /// <summary>
-    /// Метод Post контроллера Base
-    /// </summary>
-    /// <param name="id">post Идентификтор сущности</param>
-    /// <returns></returns>
-    [HttpPost]
-    public async Task<IActionResult> PostAsync(int id)
-    {
-        await HttpContext.RequestServices.GetRequiredService<IPostService>().HandleAsync(id);
-        return Ok(id);
-    }
 }
